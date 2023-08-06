8dd5abd9f41deec931640b8959f50554c5d5b1d0
AnastasiyaNovikava <kot.22.sui.66@gmail.com>
2023-08-06 22:11:02 +0300

    ++++file

--- src/credit11/Main.java
+++ src/credit11/Main.java
@@ -1,6 +1,22 @@
 package credit11;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 //Создать файл с текстом, прочитать, подсчитать в тексте количество знаков препинания и слов.
 public class Main {
+    public static void main(String[] args) {
 
+
+        try {
+            FileWriter file = new FileWriter("text.txt");
+            file.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+    }
 
 }
