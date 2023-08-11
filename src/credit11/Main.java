69dc2446b44078e872a402d8c8cffd6a792fab8b
AnastasiyaNovikava <kot.22.sui.66@gmail.com>
2023-08-11 15:57:50 +0300

    text

--- src/credit11/Main.java
+++ src/credit11/Main.java
@@ -1,22 +1,47 @@
 package credit11;
-
-import java.io.FileWriter;
-import java.io.IOException;
-
 //Создать файл с текстом, прочитать, подсчитать в тексте количество знаков препинания и слов.
+import java.io.*;
+
+
+
 public class Main {
     public static void main(String[] args) {
-
-
         try {
             FileWriter file = new FileWriter("text.txt");
+//            file.write("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n");
+//            file.write("Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.\n");
+//            file.write("The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.");
+            file.write("Мама мыла раму, если солнечно. Все будет хорошо, я узнавала.");
+            file.write(" Aevv, wfe: dvev. ");
             file.close();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
 
 
-
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("text.txt"));
+            String line;
+            int countOfWords = 0;
+            while((line = bufferedReader.readLine()) != null)
+            {
+                System.out.println(line);
+                String[] words = line.split("\b");
+                int countOfWordInLine = 0;
+                for(String word : words) {
+                    System.out.println(word);
+                    if (!word.isEmpty()) countOfWordInLine++;
+                }
+                countOfWords += countOfWordInLine;
+            }
+            System.out.println("кол-во слов = "+countOfWords);
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
+
